e126f9f368a8f2fb970e8d8dbcf534bd53a9e494
Steven Yan <steven_nagisa@qq.com>
2022-11-08 00:33:21 +0800

    😊use content.js to zoom page at startup

--- src/ts/background.ts
+++ src/ts/background.ts
@@ -4,3 +4,39 @@ chrome.runtime.onInstalled.addListener(async () => {
     "font-size:24px;color:white;background-color:blue;padding:4px;"
   );
 });
+
+chrome.tabs.onZoomChange.addListener((e) => {
+  const zoom = Math.round(e.newZoomFactor * 100);
+  console.log("Manual zoom:", zoom);
+  const tabId = e.tabId;
+  chrome.action.setBadgeBackgroundColor({ color: "#fff" });
+  chrome.action.setBadgeText({
+    text: `${zoom}`,
+    tabId,
+  });
+});
+
+chrome.runtime.onMessage.addListener((e) => {
+  console.log("onMessage", e);
+  chrome.storage.sync.get(["zoom-tool-set"], (data) => {
+    let num = Number.parseInt(data["zoom-tool-set"]);
+    const zoom = num >= 10 && num <= 300 ? num : 100;
+    console.log("Current zoom:", zoom);
+    chrome.tabs.query({ active: true }, function (tabs) {
+      const tabId = tabs[0].id;
+      if (/^(f|ht)tps?:\/\//i.test(tabs[0].url)) {
+        chrome.tabs.getZoom(tabId, (e) => {
+          if (Math.round(e * 100) !== zoom) {
+            chrome.tabs.setZoom(tabId, zoom / 100);
+            chrome.tabs.setZoomSettings(tabId, { scope: "per-tab" });
+          }
+        });
+        chrome.action.setBadgeBackgroundColor({ color: "#fff" });
+        chrome.action.setBadgeText({
+          text: `${zoom}`,
+          tabId,
+        });
+      }
+    });
+  });
+});
