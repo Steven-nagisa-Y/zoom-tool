f2a2f5e2b4423eb987fa300131c6d4c27f0fdc41
Steven Yan <steven_nagisa@qq.com>
2022-11-07 22:41:26 +0800

    😊First commit

---
+++ src/ts/background.ts
@@ -0,0 +1,6 @@
+chrome.runtime.onInstalled.addListener(async () => {
+  console.log(
+    "%cExtension installed!",
+    "font-size:24px;color:white;background-color:blue;padding:4px;"
+  );
+});
---
+++ src/ts/popup.ts
@@ -0,0 +1,101 @@
+let zoom = 100;
+const zoomShow = document.getElementById("zoom");
+const elZoom = document.getElementById("el-zoom") as HTMLInputElement;
+const plusZoom = document.getElementById("plus-zoom");
+const minusZoom = document.getElementById("minus-zoom");
+const resetZoom = document.getElementById("reset-zoom");
+const applyAll = document.getElementById("apply-all");
+
+function load() {
+  chrome.storage.sync.get(["zoom-tool-set"], (res) => {
+    let num = Number.parseInt(res["zoom-tool-set"]);
+    zoom = num >= 10 && num <= 300 ? num : 100;
+    setAll(zoom);
+  });
+}
+
+window.addEventListener("load", () => {
+  load();
+});
+
+chrome.tabs.onZoomChange.addListener((e) => {
+  console.log(e);
+  zoom = Math.round(e.newZoomFactor * 100);
+  zoomShow.innerHTML = `${zoom}%`;
+  elZoom.value = zoom.toString();
+  chrome.storage.sync.set({ "zoom-tool-set": zoom }, () => {
+    console.log(`Set zoom-tool-set to ${zoom}`);
+  });
+  chrome.tabs.query({ active: true }, function (tabs) {
+    const tabId = tabs[0].id;
+    if (/^(f|ht)tps?:\/\//i.test(tabs[0].url)) {
+      chrome.action.setBadgeBackgroundColor({ color: "#fff" });
+      chrome.action.setBadgeText({
+        text: `${zoom}`,
+        tabId,
+      });
+    }
+  });
+});
+
+function setAll(zoom: number) {
+  zoomShow.innerHTML = `${zoom}%`;
+  elZoom.value = zoom.toString();
+  chrome.storage.sync.set({ "zoom-tool-set": zoom }, () => {
+    console.log(`Set zoom-tool-set to ${zoom}`);
+  });
+  console.log(zoom / 100);
+  chrome.tabs.query({ active: true }, function (tabs) {
+    const tabId = tabs[0].id;
+    if (/^(f|ht)tps?:\/\//i.test(tabs[0].url)) {
+      chrome.tabs.setZoom(tabId, zoom / 100);
+      chrome.tabs.setZoomSettings(tabId, { scope: "per-tab" });
+      chrome.action.setBadgeBackgroundColor({ color: "#fff" });
+      chrome.action.setBadgeText({
+        text: `${zoom}`,
+        tabId,
+      });
+    }
+  });
+}
+
+elZoom.addEventListener("change", (e) => {
+  zoom = Number.parseInt((e.target as HTMLInputElement).value);
+  setAll(zoom);
+  e.stopPropagation();
+});
+
+plusZoom.addEventListener("click", (e) => {
+  zoom = zoom < 200 ? zoom + 25 : zoom;
+  setAll(zoom);
+  e.stopPropagation();
+});
+
+minusZoom.addEventListener("click", (e) => {
+  zoom = zoom > 25 ? zoom - 25 : zoom;
+  setAll(zoom);
+  e.stopPropagation();
+});
+
+resetZoom.addEventListener("click", (e) => {
+  zoom = 100;
+  setAll(zoom);
+  e.stopPropagation();
+});
+
+applyAll.addEventListener("click", (e) => {
+  chrome.tabs.query({ currentWindow: true }, function (tabs) {
+    for (const t of tabs) {
+      if (/^(f|ht)tps?:\/\//i.test(t.url)) {
+        chrome.tabs.setZoom(t.id, zoom / 100);
+        chrome.tabs.setZoomSettings(t.id, { scope: "per-tab" });
+        chrome.action.setBadgeBackgroundColor({ color: "#fff" });
+        chrome.action.setBadgeText({
+          text: `${zoom}`,
+          tabId: t.id,
+        });
+      }
+    }
+  });
+  e.stopPropagation();
+});
